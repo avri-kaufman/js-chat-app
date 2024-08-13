42d7bc4201d2c297f4892b8613c171613bbae8e5
avi <avrikaufman@gmail.com>
2024-08-13 14:56:55 +0300

    added list of conected users-need to remove user whene connection close

--- serverSide/chatServer.js
+++ serverSide/chatServer.js
@@ -5,30 +5,25 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.use(express.urlencoded({ extended: true }));
-
 app.get("/", (req, res) => {
   res.sendFile(
     "C:/Users/avika/Desktop/gettJob/projects/chatApplication/clientSide/pages/index.html"
   );
 });
 
+app.use(express.urlencoded({ extended: true }));
 app.post("/chat", (req, res) => {
   const name = req.body.name;
-  console.log(name);
   res.sendFile(
     "C:/Users/avika/Desktop/gettJob/projects/chatApplication/clientSide/pages/chat.html"
   );
+
+  io.once("connection", (socket) => {
+    socket.broadcast.emit("user connected", `${name}`);
+  });
 });
 
-// app.get("/chat", (req, res) => {
-//   res.sendFile(
-//     "C:/Users/avika/Desktop/gettJob/projects/chatApplication/clientSide/pages/chat.html"
-//   );
-// });
-
 io.on("connection", (Socket) => {
-  Socket.broadcast.emit("chat message", "new user connected");
   Socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
   });
