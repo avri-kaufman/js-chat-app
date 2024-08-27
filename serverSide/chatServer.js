8ab04d52fb2c21a67a3c48813386b016ff11dd88
avi <avrikaufman@gmail.com>
2024-08-27 11:49:23 +0300

    fix typing feacher

--- serverSide/chatServer.js
+++ serverSide/chatServer.js
@@ -34,13 +34,15 @@ app.post("/chat", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  socket.on("user typing", () =>
+  socket.on("user typing", () => {
+    console.log("got the user typing");
     socket.broadcast.emit("user typing", {
       id: socket.id,
       name: CONNECTED_USERS[socket.id],
       msg: "typing...",
-    })
-  );
+    });
+    console.log(socket.id, "typing");
+  });
 
   socket.on("chat message", (msg) => {
     socket.broadcast.emit("chat message", {
